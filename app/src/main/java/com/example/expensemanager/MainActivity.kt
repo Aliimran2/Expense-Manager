e7917d4503bbc445dae73e2cbbb0bd9a2eae78bb
aliimran2 <muhammadimran.math@gmail.com>
2024-10-06 16:53:02 +0500

    initial commit

---
+++ app/src/main/java/com/example/expensemanager/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.expensemanager
+
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import com.example.expensemanager.databinding.ActivityMainBinding
+
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.title = "Transactions"
+
+        binding.floatingActionButton.setOnClickListener {
+            AddTransactionFragment().show(supportFragmentManager, null)
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+
+    }
+}
